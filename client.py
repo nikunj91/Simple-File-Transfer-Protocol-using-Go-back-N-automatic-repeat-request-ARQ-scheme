8767896e1a3c51629b55f27914170a072a6c41b2
nikunj91 <shah_nikunj91@hotmail.com>
2016-04-16 18:29:05 -0400

    Checkpoint 1 - Coding Complete - Testing remaining

--- client.py
+++ client.py
@@ -2,51 +2,114 @@
 
 import sys
 import collections
+import pickle
+import signal
+import threading
+from multiprocessing import Lock
+from collections import namedtuple
 
 #Variables
-ACK = 0 # ACK received from server.
+N = 0
+RTT = 0.1
+TYPE_DATA = "0101010101010101"
+TYPE_ACK = "1010101010101010"
+TYPE_EOF = "1111111111111111"
+ACK_HOST = socket.gethostname()
+ACK_PORT = 65000
+max_seq_number=0
+last_ack_packet = -1
+last_send_packet = -1 # ACK received from server.
 sliding_window = set() #Ordered dictionary
 client_buffer = collections.OrderedDict() #Ordered dictionary
-N = 0
+thread_lock = Lock()
+data_packet = namedtuple('data_packet', 'sequence_no checksum type data')
+ack_packet = namedtuple('ack_packet', 'sequence_no padding type')
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sending_completed=False
 
-def receive_ack():
-	print "received"
-	return 0
-	
-def send_packet(client_buffer(last_send_packet+1, host, port):
-	print "packet sent"
+SEND_HOST = sys.argv[1]
+SEND_PORT = sys.argv[2]	
+FILE_NAME = sys.argv[3]
+N = sys.argv[4]	
+MSS = sys.argv[5]
 
-def send_file(file_content, client_socket, host, port):
-	last_send_packet = -1
-	last_ack_packet = -1
-	while len(client_buffer) != 0:
-		while len(sliding_window) < N:
-			send_packet(client_buffer(last_send_packet+1, host, port)
+def retransmit_packet(packet, host, port, socket, sequence_no):
+	global last_ack_packet
+	if last_ack_packet<sequence_no:
+		print "packet "+sequence_no+" timer expired"
+		send_packet(packet, host, port, socket, sequence_no)
+
+
+def send_packet(packet, host, port, socket, sequence_no):
+	socket.sendto(packet, (host, port))
+	t=threading.Timer(RTT,retransmit_packet,[packet,host,port,socket,sequence_no])
+	t.start()
+	print "packet "+sequence_no+" sent"
+
+def rdt_send(file_content, client_socket, host, port):
+	global last_send_packet,last_ack_packet,sliding_window,client_buffer
+	while len(sliding_window)<min(len(client_buffer),N-1):
+		if last_ack_packet==-1:
+			send_packet(client_buffer[last_send_packet+1], host, port, client_socket, last_send_packet+1)
 			last_send_packet = last_send_packet + 1
 			sliding_window.add(last_send_packet)
-		while 1:
-			temp = receive_ack()
-			for i in range(last_ack_packet+1,temp+1):
-				sliding_window.remove(i)
-				buffer.pop(i)
-				last_ack_packet = temp
 	
+def compute_checksum_for_chuck(chunk):
+	checksum=0
+	l=len(chunk)
+	chunk=str(chunk)
+	for byte in range(0,l,2):
+		byte1=ord(chunk[byte])
+		shifted_byte1=byte1<<8
+		byte2=ord(chunk[byte+1])
+		merged_bytes=shifted_byte1+byte2
+		checksum_add=checksum+merged_bytes
+		carryover=checksum_add>>16
+		main_part=checksum_add&0xffff
+		checksum=main_part+carryover
+	checksum_complement=checksum^0xffff
+	return checksum_complement
+
+def ack_process:
+	global last_ack_packet,last_send_packet,client_buffer,sliding_window,client_socket,SEND_PORT,SEND_HOST
+	ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ack_socket.bind((ACK_HOST, ACK_PORT))
+    while 1:
+    	reply = pickle.loads(ack_socket.recv(65535))
+    	if reply[2] == TYPE_ACK:
+    		current_ack_seq_number=reply[0]-1
+    		if last_ack_packet >= -1:
+    			thread_lock.acquire()
+    			if current_ack_seq_number == max_seq_number:
+    				eof_packet = pickle.dumps(["0", "0", TYPE_EOF, "0"])
+                    client_socket.sendto(eof_packet, (SERVER_HOST, SERVER_PORT))
+                    thread_lock.release()
+                    sending_completed=True
+                    break
+        		elif current_ack_seq_number>last_ack_packet:
+        			while last_ack_packet<=current_ack_seq_number:
+        				last_ack_packet=last_ack_packet+1
+        				sliding_window.pop(last_ack_packet)
+        				client_buffer.pop(last_ack_packet)
+        				if last_send_packet<max_seq_number:
+        					send_packet(client_buffer[last_send_packet+1],SEND_HOST,SEND_PORT,client_socket,last_send_packet+1)
+        					last_send_packet=last_send_packet+1
+        			thread_lock.release()
+
+
+
+
 def main():
-	global client_buffer 
-	global N
+	global client_buffer ,max_seq_number,client_socket,N
 	
-	host = sys.argv[1]
-    port = sys.argv[2]	
-    file_name = sys.argv[3]
-    N = sys.argv[4]	
-    mss = sys.argv[5]
+
 	
-	port = int(port)
+	port = int(SEND_PORT)
+	host = SEND_HOST
 	N = int(N)
-	mss = int(mss)
+	mss = int(MSS)
 	
-	#UDP datagram socket
-	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	#UDP datagram socke
 	
 	max_window_size = N - 1
 	
@@ -56,15 +119,21 @@ def main():
             while True:
                 chunk = f.read(int(MSS))  
                 if chunk:
-                    client_buffer[sequence_number] = chunk
+                	max_seq_number=sequence_number
+                	chunk_checksum=compute_checksum_for_chuck(chunk)
+                    client_buffer[sequence_number] = pickle.dumps([sequence_number,chunk_checksum,TYPE_DATA,chunk])
+                    sequence_number=sequence_number+1
                 else:
                     break
-        #print(file_content)
     except:
         sys.exit("Failed to open file!")
-		
-	send_file(client_buffer, client_socket, host, port)
-	
+
+	ack_thread = threading.Thread(target=ack_process)
+	ack_thread.start() 	
+	rdt_send(client_buffer, client_socket, host, port)
+	while 1:
+		if sending_completed:
+			break
 	
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -2,20 +2,72 @@
 
 import sys
 
+SERVER_PORT = int(sys.argv[1])
+FILE_NAME = sys.argv[2]
+PACKET_LOSS_PROB = float(sys.argv[3])
+TYPE_DATA = "0101010101010101"
+TYPE_ACK = "1010101010101010"
+TYPE_EOF = "1111111111111111"
+DATA_PAD = "0000000000000000"
+ACK_PORT = 65000
+HOST_NAME = socket.gethostname()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST_NAME, SERVER_PORT))
+last_received_packet=-1
+
+def compute_checksum_for_chuck(chunk,checksum):
+	l=len(chunk)
+	chunk=str(chunk)
+	for byte in range(0,l,2):
+		byte1=ord(chunk[byte])
+		shifted_byte1=byte1<<8
+		byte2=ord(chunk[byte+1])
+		merged_bytes=shifted_byte1+byte2
+		checksum_add=checksum+merged_bytes
+		carryover=checksum_add>>16
+		main_part=checksum_add&0xffff
+		checksum=main_part+carryover
+	checksum_complement=checksum^0xffff
+	return checksum_complement
+
+def is_checksum_proper(chunk,checksum):
+	return compute_checksum_for_chuck(chunk,checksum)==0: 
+
+def check_if_packet_drop(PACKET_LOSS_PROB,packet_sequence_number):
+	return random.random()<PACKET_LOSS_PROB:
+
+def send_acknowledgement(ack_number):
+	ack_packet = pickle.dumps([acknowledgement, DATA_PAD, TYPE_ACK])
+    ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ack_socket.sendto(ack_packet,(HOST_NAME, ACK_PORT))
+    ack_socket.close()
+
+def write_data_to_file(packet_data):
+	with open(FILE_NAME, 'ab') as file:
+		file.write(packet_data)
+
 def main():
-	# Read command line argument
-	port = sys.argv[1]
-    file_name = sys.argv[2]
-    probability = sys.argv[3]
-	
-	port = int(port)
-	probability = float(probability)
-	
-	#UDP socket
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	host = socket.gethostname()
-	server_socket.bind((host, port)) 
-	#while True:
-		#data, addr = server_socket.recvfrom(1000000)
-        #data = pickle.loads(data)
-		#print data
+	global last_received_packet
+	while True:
+		received_data, addr = server_socket.recvfrom(65535)
+        received_data = pickle.loads(received_data)
+		packet_sequence_number, packet_checksum, packet_type, packet_data = received_data[0], received_data[1], received_data[2], received_data[3]
+		if packet_type == TYPE_EOF:
+			print("File Received from client..Yaaaaaaaay")
+			server_socket.close()
+			break
+		elif packet_type == TYPE_DATA:
+			drop_packet=check_if_packet_drop(PACKET_LOSS_PROB,packet_sequence_number)\
+			if drop_packet:
+				print "Packet "+packet_sequence_number+" has been dropped due to less probability"
+			elif is_checksum_proper(packet_data,packet_checksum):
+				if packet_sequence_number==last_received_packet+1:
+					send_acknowledgement(packet_sequence_number+1)
+					last_received_packet=last_received_packet+1
+					write_data_to_file(packet_data)
+				else:
+					send_acknowledgement(last_received_packet+1)
+			else:
+				print "Packet "+packet_sequence_number+" has been dropped due to improper checksum"
+
+
