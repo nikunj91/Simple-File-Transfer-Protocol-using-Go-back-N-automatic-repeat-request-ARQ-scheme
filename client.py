99d38ba49ed644bd27e1e82eacfc83e8f9a307aa
nikunj91 <shah_nikunj91@hotmail.com>
2016-04-20 02:20:09 -0400

    Working GBackN Code with one server and one client

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
-#Simple-FTP sender
+#!/usr/bin/python           
+#This is the client code
+#Author Nikunj Shah
+#Author Aparna Patil
+
 import socket
 import sys
 import collections
@@ -9,182 +13,222 @@ from multiprocessing import Lock
 from collections import namedtuple
 import time
 
-#Variables
-N = 0
-RTT = 0.1
+#Constant Declarations
+
+RTT = 0.1 # Assumed value for timeouts
+
+#Defining the types of packets
 TYPE_DATA = "0101010101010101"
 TYPE_ACK = "1010101010101010"
 TYPE_EOF = "1111111111111111"
-ACK_HOST = '10.139.63.147'
-ACK_PORT = 65000
-max_seq_number=0
-last_ack_packet = -1
-last_send_packet = -1 # ACK received from server.
-sliding_window = set() #Ordered dictionary
-client_buffer = collections.OrderedDict() #Ordered dictionary
-thread_lock = Lock()
+ACK_HOST = socket.gethostbyname(socket.gethostname())
+print ACK_HOST
+ACK_PORT = 65000 
+
+#Variable Declaration
+max_seq_number=0 #sequence number of the last packet
+last_ack_packet = -1  # ACK received from server.
+last_send_packet = -1  # Last packet sequence number sent to the server
+sliding_window = set() # Maintain the Slding window at the sender end
+client_buffer = collections.OrderedDict() # Will act as a buffer and keep all the packets that are yet to be sent to the receiver
+
+#Defining the namedtuples that will work as a complete packet including header and data that will be used for packet exchanges 
 data_packet = namedtuple('data_packet', 'sequence_no checksum type data')
 ack_packet = namedtuple('ack_packet', 'sequence_no padding type')
+
+#Initially thread lock will be acquired here
+thread_lock = Lock()
+
+#Client end socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Will keep a track whether sending has completed or not
 sending_completed=False
 
+#Maintain the time to be used for report generation
 t_start=0
 t_end=0
 
-SEND_HOST = '152.14.142.101'#socket.gethostbyname(socket.gethostname())
-SEND_PORT = 7735
+#Values taken from the command line
+#Host of the server
+SEND_HOST = sys.argv[1]
+#Server Port
+SEND_PORT = int(sys.argv[2])
+#File Name
 FILE_NAME = sys.argv[3]
-N = sys.argv[4]	
-MSS = sys.argv[5]
+#Window Size
+N = int(sys.argv[4])
+#MSS Size
+MSS = int(sys.argv[5])
 
+#Send the packet to the host details in the parameters
 def send_packet(packet, host, port, socket, sequence_no):
 	client_socket.sendto(packet,(SEND_HOST,SEND_PORT)) #(SEND_HOST, SEND_PORT))
-	print "packet "+str(sequence_no)+" sent"
 
+#Will handle the initial packet sending till the receipt of the acknowledgement from the receiver or sliding window is full
 def rdt_send(file_content, client_socket, host, port):
 	global last_send_packet,last_ack_packet,sliding_window,client_buffer,t_start
-	print host
-	print port
+	#Timer starts as first packet is send from here
 	t_start=time.time()
+	#Send packets till the sliding window is full or all packets are sent
 	while len(sliding_window)<min(len(client_buffer),N):
+		#Till the time no acknowledgement arrives from receiver
 		if last_ack_packet==-1:
+			#Send the packet
 			send_packet(client_buffer[last_send_packet+1], host, port, client_socket, last_send_packet+1)
-			print 'rdt sent '+str(last_send_packet+1)
+			#Set the timer for retransmission
 			signal.alarm(0)
  			signal.setitimer(signal.ITIMER_REAL, RTT)
+ 			#Increment the packet sequence number
 			last_send_packet = last_send_packet + 1
+			#Add the packet to the sliding window
 			sliding_window.add(last_send_packet)
 			y=0
 			while y<100000:
 				y=y+1
-	print 'rdt done'
-	
+
+#Will compute the checksum for the chunk of data provided
 def compute_checksum_for_chuck(chunk):
 	checksum=0
 	l=len(chunk)
-	#print l
 	chunk=str(chunk)
 	byte=0
-	#print 'ooooo'
 	while byte<l:
-		#print byte
+		#Take 2 bytes of from the chunk data...takes 0xffff if the byte2 is not there because of odd chunk size
 		byte1=ord(chunk[byte])
 		shifted_byte1=byte1<<8
 		if byte+1==l:
-			#print 'lllllll'
 			byte2=0xffff
 		else:
 			byte2=ord(chunk[byte+1])
+		#Merge the bytes in the form of byte1byte2 to make 16bits
 		merged_bytes=shifted_byte1+byte2
+		#Add to the 16 bit chekcsum computed till now
 		checksum_add=checksum+merged_bytes
+		#Compute the carryover
 		carryover=checksum_add>>16
+		#Compute the main part of the new checksum
 		main_part=checksum_add&0xffff
+		#Add the carryover to the main checksum again
 		checksum=main_part+carryover
+		#Do same for next 2 bytes
 		byte=byte+2
+	#Take 1's complement of the computed checksum and return it
 	checksum_complement=checksum^0xffff
 	return checksum_complement
 
+#will monitor the incoming ack's and sent the remaining packets
 def ack_process():
 	global last_ack_packet,last_send_packet,client_buffer,sliding_window,client_socket,SEND_PORT,SEND_HOST,sending_completed,t_end,t_start,t_total
+	#Create the server socket that will listen for incoming ack's
 	ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ack_socket.bind((ACK_HOST, ACK_PORT))
+
 	while 1:
-		print 'waiting'
+		#Receive the ACK
 		reply = pickle.loads(ack_socket.recv(65535))
-		print 'got ack for '+str(reply[0]-1)
+		#If it incoming packet is an ACK
 		if reply[2] == TYPE_ACK:
-			print 'processing ack for '+str(reply[0]-1)
+			#Retrieve the sequence number in the ACK to get the last ACK'ed packet
 			current_ack_seq_number=reply[0]-1
 			if last_ack_packet >= -1:
-				print 'ack'
+				#Acquire the thread control
 				thread_lock.acquire()
-				print 'ack lock acq for '+str(reply[0]-1)
-				print 'last ack packet ' + str(last_ack_packet)
+				#If this is the last ACK, send the EOF Packet to receiver
+				#Release the thread control
+				#Maintain the end time
     			if current_ack_seq_number == max_seq_number:
+    				#Create the EOF Packet and send it
     				eof_packet = pickle.dumps(["0", "0", TYPE_EOF, "0"])
     				client_socket.sendto(eof_packet, (SEND_HOST, SEND_PORT))
+    				#Release the thread
     				thread_lock.release()
-    				print 'lock rel up'+str(reply[0]-1)
+    				#Mark sending as complete
     				sending_completed=True
+    				#Record the end time of the transfer
     				t_end=time.time()
-    				print "Start Time: "+str(t_start)
-    				print "End Time: "+str(t_end)
     				t_total=t_end-t_start
-    				print "Total Time: "+str(t_total)
-    				with open('time.txt', 'ab') as file:
-    					file.write(str(t_total)+'\n')
     				break
+    			#Else if not the last ACK and also not ACK of a packet already ACK'ed by a higher sequence number ACK
     			elif current_ack_seq_number>last_ack_packet:
-    				print 'inside ack accepting for packet '+str(reply[0]-1)
+    				# if new ACK recieved
         			while last_ack_packet<current_ack_seq_number:
+        				#Reset the timer again
         				signal.alarm(0)
         				signal.setitimer(signal.ITIMER_REAL, RTT)
+        				#Reset the value of last receiveed ack
         				last_ack_packet=last_ack_packet+1
-        				print sliding_window
+        				#Remvove the ACK'ed packet from the sliding window and the buffer
         				sliding_window.remove(last_ack_packet)
         				client_buffer.pop(last_ack_packet)
+        				#Send the packets till the sliding window has space
         				while len(sliding_window)<min(len(client_buffer),N):
 	        				if last_send_packet<max_seq_number:
 	        					send_packet(client_buffer[last_send_packet+1],SEND_HOST,SEND_PORT,client_socket,last_send_packet+1)
 	        					sliding_window.add(last_send_packet+1)
 	        					last_send_packet=last_send_packet+1
+	        		#Release the thread lock
         			thread_lock.release()
-        			print 'lock rel mid'+str(reply[0]-1)
         		else:
         			thread_lock.release()
-        			print 'lock rel down'+str(reply[0]-1)
-        	print'done again'
 
+#Handle the timeouts
 def timeout_thread(timeout_th, frame):
 	global last_ack_packet
  	if last_ack_packet==last_send_packet-len(sliding_window):
- 		print "packet "+str(last_ack_packet+1)+" timer expired"
+ 		print "Timeout, sequence number = "+str(last_ack_packet+1)
+ 		#Acquire the thread lock
  		thread_lock.acquire()
+ 		#Resend all the packets in the sliding window from the timeout packet
  		for i in range(last_ack_packet+1,last_ack_packet+1+len(sliding_window),1):
  			signal.alarm(0)
  			signal.setitimer(signal.ITIMER_REAL, RTT)
 			send_packet(client_buffer[i], SEND_HOST, SEND_PORT, client_socket, i)
+		#Release the thread lock
 		thread_lock.release()
 	
-
+#Main function
 def main():
 	global client_buffer ,max_seq_number,client_socket,N,SEND_PORT,SEND_HOST,MSS
 
+	#Variable intialization
 	port = SEND_PORT
 	host = SEND_HOST
 	N = int(N)
 	mss = int(MSS)
 	
-	#UDP datagram socke
-	
-	max_window_size = N - 1
-	
+	#Read the data from the file to be sent over and break it into chunks based on MSS size provided
+	#Store the chunkks in client_buffer based on the sequence number
+	#Calculate the last sequence number
 	sequence_number = 0
 	try:
 		with open(FILE_NAME, 'rb') as f:
 			while True:
-				print 'here'
 				chunk = f.read(int(mss))  
 				if chunk:
 					max_seq_number=sequence_number
 					chunk_checksum=compute_checksum_for_chuck(chunk)
 					client_buffer[sequence_number] = pickle.dumps([sequence_number,chunk_checksum,TYPE_DATA,chunk])
-					print max_seq_number
 					sequence_number=sequence_number+1
 				else:
 					break
 	except:
 		sys.exit("Failed to open file!")
 
+	#Initialize the signal thread that will help in timeout tracking
 	signal.signal(signal.SIGALRM, timeout_thread)
+	#Initialize the ack thread that will monitor the incoming ack's and sent the remaining packets
 	ack_thread = threading.Thread(target=ack_process)
 	ack_thread.start() 	
+	#Do initial packet sending till the receipt of the acknowledgement from the receiver or sliding window is full
 	rdt_send(client_buffer, client_socket, host, port)
+	#Monitor if the sending is complete
 	while 1:
 		if sending_completed:
 			break
-	
+	#blocks the main thread until the ack thread is terminated
 	ack_thread.join()
+	#close the socket
 	client_socket.close()
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,95 +1,116 @@
-#Receiver side of the Go-back-N protocol
+#!/usr/bin/python           
+#This is the server code
+#Author Nikunj Shah
+#Author Aparna Patil
+
 import socket
 import pickle
 import random
 import sys
 import os
 
+#Constant Declarations
+
+#Values taken from the command line
 SERVER_PORT = int(sys.argv[1])
 FILE_NAME = sys.argv[2]
 PACKET_LOSS_PROB = float(sys.argv[3])
+
+#Constant Declarations
 TYPE_DATA = "0101010101010101"
 TYPE_ACK = "1010101010101010"
 TYPE_EOF = "1111111111111111"
 DATA_PAD = "0000000000000000"
+
 ACK_PORT = 65000
-HOST_NAME = '10.139.63.147'
-ACK_HOST_NAME = '152.14.142.101'
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+HOST_NAME = '0.0.0.0'
+ACK_HOST_NAME = ''
 server_socket.bind((HOST_NAME, SERVER_PORT))
 last_received_packet=-1
+
+#if file already exists, remove the file 
 if os.path.isfile(FILE_NAME):
 	os.remove(FILE_NAME)
+
+#Will compute the checksum for the chunk of data provided
 def compute_checksum_for_chuck(chunk,checksum):
 	l=len(chunk)
 	byte=0
-	#print 'ooooo'
+	#Take 2 bytes of from the chunk data...takes 0xffff if the byte2 is not there because of odd chunk size
 	while byte<l:
-		#print byte
 		byte1=ord(chunk[byte])
 		shifted_byte1=byte1<<8
 		if byte+1==l:
-			#print 'lllllll'
 			byte2=0xffff
 		else:
 			byte2=ord(chunk[byte+1])
+		#Merge the bytes in the form of byte1byte2 to make 16bits
 		merged_bytes=shifted_byte1+byte2
+		#Add to the 16 bit chekcsum computed till now
 		checksum_add=checksum+merged_bytes
+		#Compute the carryover
 		carryover=checksum_add>>16
+		#Compute the main part of the new checksum
 		main_part=checksum_add&0xffff
+		#Add the carryover to the main checksum again
 		checksum=main_part+carryover
+		#Do same for next 2 bytes
 		byte=byte+2
+	#Take 1's complement of the computed checksum and return it
 	checksum_complement=checksum^0xffff
 	return checksum_complement
 
+#Check if checksum is proper
 def is_checksum_proper(chunk,checksum):
 	return compute_checksum_for_chuck(chunk,checksum)==0
 
+#Drop packet based on random probability generated
 def check_if_packet_drop(PACKET_LOSS_PROB,packet_sequence_number):
 	return random.random()<PACKET_LOSS_PROB
 
+#Send the ack back to the sender
 def send_acknowledgement(ack_number):
-	print "ack "+str(ack_number)+" sent"
 	ack_packet = pickle.dumps([ack_number, DATA_PAD, TYPE_ACK])
 	ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ack_socket.sendto(ack_packet,(ACK_HOST_NAME, ACK_PORT))
 	ack_socket.close()
 
+#Write the data to the file
 def write_data_to_file(packet_data):
 	with open(FILE_NAME, 'ab') as file:
 		file.write(packet_data)
 
+#Main function
 def main():
-	global last_received_packet
+	global last_received_packet, ACK_HOST_NAME
 	completed=False
-	#print 'inside main'
 	while not completed:
-		print 'while'
+		#Receive data from the sender
 		received_data1, addr = server_socket.recvfrom(65535)
-		print 'got'
-		print 'DATA RECEIVED'
-		print addr
-		#print received_data1
-		#print received_data1
+		ACK_HOST_NAME = addr[0]
 		received_data = pickle.loads(received_data1)
-		print 'Split Kiya'
 		packet_sequence_number, packet_checksum, packet_type, packet_data = received_data[0], received_data[1], received_data[2], received_data[3]
+		#Check if the packet is last packet
 		if packet_type == TYPE_EOF:
-			print("File Received from client..Yaaaaaaaay")
 			completed=True
 			server_socket.close()
 		elif packet_type == TYPE_DATA:
-			print("Packet "+ str(packet_sequence_number)+ " received")
+			#Check if packet needs to be dropped due to probability
 			drop_packet=check_if_packet_drop(PACKET_LOSS_PROB,packet_sequence_number)
 			if drop_packet==True:
-				print "Packet "+str(packet_sequence_number)+" has been dropped due to less probability"
+				print "Packet loss, sequence number = "+str(packet_sequence_number)
 			else:
+				#Check if the checksum is proper
 				if is_checksum_proper(packet_data,packet_checksum):
+					#If the iincoming packet has arrived in desired order, then send ACK
 					if packet_sequence_number==last_received_packet+1:
 						send_acknowledgement(packet_sequence_number+1)
 						last_received_packet=last_received_packet+1
+						#Write the data to the file
 						write_data_to_file(packet_data)
 					else:
+						#Else send the ACK of the expected packet
 						send_acknowledgement(last_received_packet+1)
 				else:
 					print "Packet "+str(packet_sequence_number)+" has been dropped due to improper checksum"
