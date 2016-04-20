73c03d8afa3ce96401e3ae5156192db140b96aac
nikunj91 <shah_nikunj91@hotmail.com>
2016-04-20 03:13:42 -0400

    Added 0.0.0.0 as server host address for other network discovery

--- client.py
+++ client.py
@@ -21,7 +21,7 @@ RTT = 0.1 # Assumed value for timeouts
 TYPE_DATA = "0101010101010101"
 TYPE_ACK = "1010101010101010"
 TYPE_EOF = "1111111111111111"
-ACK_HOST = socket.gethostbyname(socket.gethostname())
+ACK_HOST = '0.0.0.0'
 print ACK_HOST
 ACK_PORT = 65000 
 
